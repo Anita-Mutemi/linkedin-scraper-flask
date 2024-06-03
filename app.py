3187caa67220be9a7c96f144b7b459224ece4a49
Anita-Mutemi <anitajehu13@gmail.com>
2024-06-03 23:33:32 +0300

    add host and port

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ def bulk_delete_likers():
     return jsonify({"message": "Likers deleted successfully"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
